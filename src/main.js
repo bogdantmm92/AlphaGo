78801af8f40454e8e66017108e2742d9e35ed73c
Bogdan Tirca <bogdantmm@MacBook-Pro.fios-router.home>
2017-03-19 13:14:43 -0400

    Basic Login & Signup

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  AsyncStorage
 } from 'react-native';
 
 import io from 'socket.io-client';
@@ -14,17 +15,54 @@ import CONST from './const';
 export default class Main extends Component {
   constructor(props) {
     super(props);
+    this.state = {user: null};
     this.setupSocket();
   }
 
   setupSocket() {
     this.socket = io(CONST.SERVER_URL);
 
-    this.socket.on('connect', () => {
-      console.log('connected!');
+    this.socket.on('connect', this.handleConnect.bind(this));
+    this.socket.on('disconnect', this.handleDisconnect.bind(this));
+    this.socket.on('joined_pending_game', this.handlePendingGame.bind(this));
+  }
+
+  handlePendingGame() {
+    console.log('joined_pending_game');
+  }
+
+  handleDisconnect() {
+    console.log('disconnected');
+  }
+  handleConnect() {
+    console.log('connected!');
+    this.loginOrCreateUser().then((user) => {
+      this.setState({user});
     });
-    this.socket.on('disconnect', () => {
-      console.log('disconnected');
+  }
+
+  loginOrCreateUser() {
+    console.log('loginOrCreateUser');
+    return AsyncStorage.getItem('user')
+      .then((user) => {
+        if (user) {
+          user = JSON.parse(user);
+          return new Promise((resolve, reject) => {
+            this.socket.emit('login_user', user, (user) => {
+              console.log("user login");
+              AsyncStorage.setItem('user', JSON.stringify(user));
+              resolve(user);
+            });
+          });
+        } else {
+          return new Promise((resolve, reject) => {
+            this.socket.emit('create_user', (user) => {
+              console.log("user created");
+              AsyncStorage.setItem('user', JSON.stringify(user));
+              resolve(user);
+            });
+          });
+        }
     });
   }
 
